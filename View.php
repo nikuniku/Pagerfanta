644b2736be1ed673d6f7b29d75e54f04463558a1
nikuniku <nikunja_1989@yahoo.com>
2022-08-13 11:46:01 +0600

    first commit

---
+++ View.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * This file is part of the Swoopaholic Framework Bundle.
+ *
+ * (c) Danny Dörfel <danny@swoopaholic.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Swoopaholic\Bridge\Pagerfanta;
+
+class View
+{
+    /**
+     * @var array
+     */
+    public $pages = array();
+
+    /**
+     * @var int
+     */
+    public $pagesCount = 0;
+
+    /**
+     * @var int
+     */
+    public $currentPage = 1;
+
+    /**
+     * @var string
+     */
+    public $next;
+
+    /**
+     * @var string
+     */
+    public $previous;
+
+    /**
+     * @var string
+     */
+    public $first;
+
+    /**
+     * @var string
+     */
+    public $last;
+
+    /**
+     * @var
+     */
+    public $dataSlice;
+}
